9c99a275c64e9f0aa85813ccb317231fc29242d5
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-05 15:17:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -f /usr/bin/main /usr/bin/proxy
-sudo curl -sL -o /usr/bin/main https://github.com/DTunnel0/DTunnelProxyServerRelease/raw/main/main.sh
-sudo curl -sL -o /usr/bin/proxy https://github.com/DTunnel0/DTunnelProxyServerRelease/raw/main/build/$(uname -m)/proxy
+sudo curl -sL -o /usr/bin/main https://github.com/curl -s -L https://raw.githubusercontent.com/PhoenixxZ2023/DTunnelProxyServerRelease/main/install.sh | bash/DTunnelProxyServerRelease/raw/main/main.sh
+sudo curl -sL -o /usr/bin/proxy https://github.com/PhoenixxZ2023/DTunnelProxyServerRelease/raw/main/build/$(uname -m)/proxy
 sudo chmod +x /usr/bin/main /usr/bin/proxy
 clear && echo -e "\033[1;31mExecute: \033[1;32mmain\033[0m"
