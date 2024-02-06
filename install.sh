939392e7035089a63a37016ce1733545a628fe41
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-05 21:34:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
-rm -f /usr/bin/main /usr/bin/proxy
-curl -s -L -o /usr/bin/main https://github.com/PhoenixxZ2023/DTunnelProxyServerRelease/raw/main/main.sh
-curl -s -L -o /usr/bin/proxy https://github.com/PhoenixxZ2023/DTunnelProxyServerRelease/raw/main/build/$(uname -i)/proxy
-chmod +x /usr/bin/main /usr/bin/proxy
+#!/bin/bash
+
+# Define variables for URLs and file paths
+MAIN_URL="https://github.com/PhoenixxZ2023/DTunnelProxyServerRelease/raw/main/main.sh"
+PROXY_URL="https://github.com/PhoenixxZ2023/DTunnelProxyServerRelease/raw/main/build/$(uname -i)/proxy"
+MAIN_FILE="/usr/bin/main"
+PROXY_FILE="/usr/bin/proxy"
+
+# Remove existing files
+rm -f "$MAIN_FILE" "$PROXY_FILE"
+
+# Download main.sh and proxy
+curl -s -L -o "$MAIN_FILE" "$MAIN_URL"
+curl -s -L -o "$PROXY_FILE" "$PROXY_URL"
+
+# Give execute permissions
+chmod +x "$MAIN_FILE" "$PROXY_FILE"
+
+# Clear the terminal and display execution instructions
 clear && echo -e "\033[1;31mExecute: \033[1;32mmain\033[0m"
